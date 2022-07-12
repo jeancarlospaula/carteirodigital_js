6a5aca7f169c5ece9d2d383af53c7f803bd4b862
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-07-12 13:53:55 -0300

    chore: changed the function to async function

--- src/services/telegram/notifyTelegram.js
+++ src/services/telegram/notifyTelegram.js
@@ -1,8 +1,8 @@
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const notifyTelegram = ({ chatId, message }) => {
-  bot.telegram.sendMessage(chatId, message, { parse_mode: 'Markdown' })
+const notifyTelegram = async ({ chatId, message }) => {
+  await bot.telegram.sendMessage(chatId, message, { parse_mode: 'Markdown' })
     .then(_ => console.log(`Notification sent to chat ${chatId}`))
     .catch((error) => {
       console.log(`Error sending notification to chat ${chatId}`)
