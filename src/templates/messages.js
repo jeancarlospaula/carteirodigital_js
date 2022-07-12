b98272ac592093a0a08000211b9386640422d452
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-07-12 13:53:20 -0300

    chore: added username on notification messages

--- src/templates/messages.js
+++ src/templates/messages.js
@@ -1,5 +1,5 @@
 const lastUpdateMessage = ({
-  code,
+  trackingCode,
   status,
   cityOrigin,
   stateOrigin,
@@ -7,42 +7,46 @@ const lastUpdateMessage = ({
   cityDestiny,
   stateDestiny,
   typeDestiny,
-  dateUpdate
+  dateUpdate,
+  firstName
 }) => `
-*Sua encomenda se movimentoooou!* 🛵 📦\n\n
-*CÓDIGO*: ${code}\n\n
+*Sua encomenda se movimentoooou, ${firstName}!* 🛵 📦\n\n
+*CÓDIGO*: ${trackingCode}\n\n
 *STATUS*: ${status}\n\n
 *ORIGEM*: ${cityOrigin.toUpperCase()} - ${stateOrigin.toUpperCase()} (${typeOrigin})\n\n
 *DESTINO*: ${cityDestiny.toUpperCase()} - ${stateDestiny.toUpperCase()} (${typeDestiny})\n\n
 *DATA DA ATUALIZAÇÃO*: ${dateUpdate}\n\n
-*Assim que ela se movimentar novamente passou aqui para te avisar, ok? Até logo!* 😃`
+*Assim que ela se movimentar novamente, eu passo aqui para te avisar, ok? Até logo!* 😃`
 
 const orderDeliveredMessage = ({
-  code,
+  trackingCode,
   status,
   cityOrigin,
   stateOrigin,
-  dateUpdate
+  dateUpdate,
+  firstName
 }) => `
-*Toc toc, sua encomenda chegou! 📦*\n\n
-*CÓDIGO*: ${code}\n\n
-*STATUS*: ${status}\n\n
-*LOCAL*: ${cityOrigin.toUpperCase()} - ${stateOrigin.toUpperCase()}\n\n
-*DATA DA ATUALIZAÇÃO*: ${dateUpdate}`
-
-const orderPostedMessage = ({
-  code,
-  status,
-  cityOrigin,
-  stateOrigin,
-  dateUpdate
-}) => `
-*Logo sua encomenda começará a se movimentar! 📦*\n\n
-*CÓDIGO*: ${code}\n\n
+*Toc toc, sua encomenda chegou, ${firstName}! 📦*\n\n
+*CÓDIGO*: ${trackingCode}\n\n
 *STATUS*: ${status}\n\n
 *LOCAL*: ${cityOrigin.toUpperCase()} - ${stateOrigin.toUpperCase()}\n\n
 *DATA DA ATUALIZAÇÃO*: ${dateUpdate}\n\n
-*Assim que ela se movimentar novamente passou aqui para te avisar, ok? Até logo!* 😃`
+*Se quiser começar a rastrear um nova encomenda, é só digital o código dela aqui embaixo! 📫*`
+
+const orderPostedMessage = ({
+  trackingCode,
+  status,
+  cityOrigin,
+  stateOrigin,
+  dateUpdate,
+  firstName
+}) => `
+*Logo sua encomenda começará a se movimentar, ${firstName}! 📦*\n\n
+*CÓDIGO*: ${trackingCode}\n\n
+*STATUS*: ${status}\n\n
+*LOCAL*: ${cityOrigin.toUpperCase()} - ${stateOrigin.toUpperCase()}\n\n
+*DATA DA ATUALIZAÇÃO*: ${dateUpdate}\n\n
+*Assim que ela se movimentar novamente, eu passo aqui para te avisar, ok? Até logo!* 😃`
 
 module.exports = {
   lastUpdateMessage,
