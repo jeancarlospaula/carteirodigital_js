8b356d58288fedc0609bf298c12ac543035e5953
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-07-17 18:03:32 -0300

    chore: added function to checl if a order was delivered

--- src/utils/checkDeliveredOrder.js
+++ src/utils/checkDeliveredOrder.js
@@ -1,5 +1,7 @@
-const checkDeliveredOrder = message => {
-  return message && message.includes(process.env.MESSAGE_DELIVERED_ORDER)
+const checkDeliveredOrder = event => {
+  return !(event.destination.city && event.destination.state) &&
+  event.status &&
+  event.status.includes(process.env.MESSAGE_DELIVERED_ORDER)
 }
 
 module.exports = { checkDeliveredOrder }
