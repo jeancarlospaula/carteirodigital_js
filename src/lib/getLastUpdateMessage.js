0773919648f9993b17f8f0a2bed3b91fb6c94d3a
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-07-17 20:46:40 -0300

    chore: added countryName property

--- src/lib/getLastUpdateMessage.js
+++ src/lib/getLastUpdateMessage.js
@@ -4,20 +4,19 @@ const { getReplyMessage } = require('./getReplyMessage')
 const getLastUpdateMessage = ({ lastEvent, trackingCode, firstName, type }) => {
   const event = {
     status: lastEvent.status,
-    cityOrigin: lastEvent.location.city,
-    stateOrigin: lastEvent.location.state,
-    typeOrigin: lastEvent.location.type,
+    cityOrigin: lastEvent.location?.city,
+    stateOrigin: lastEvent.location?.state,
+    typeOrigin: lastEvent.location?.type,
     cityDestiny: lastEvent.destination?.city,
     stateDestiny: lastEvent.destination?.state,
     typeDestiny: lastEvent.destination?.type,
     dateUpdate: lastEvent.dateUpdate,
     trackingCode,
-    firstName
+    firstName,
+    countryName: lastEvent.location?.countryName
   }
 
-  if (type === 'MESSAGE') {
-    return getReplyMessage(event)
-  }
+  if (type === 'MESSAGE') return getReplyMessage(event)
 
   return getJobMessage(event)
 }
